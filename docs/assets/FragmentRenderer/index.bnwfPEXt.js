cc86ff50fbeb7b1854510f0fbd1178fbd63f2fee
MuddySpud <111865625+MuddySpud@users.noreply.github.com>
2026-05-17 00:30:54 +0100

    chore: release v0.0.21

--- docs/assets/FragmentRenderer/index.bnwfPEXt.js
+++ docs/assets/FragmentRenderer/index.bnwfPEXt.js
@@ -1,4 +1,4 @@
-import "./guide.C5JKrSvP.js";
+import "./guide.iQpxSdrB.js";
 import "./stepHook.BHMagoq0.js";
 (function polyfill() {
   const relList = document.createElement("link").relList;
@@ -37,4 +37,4 @@ import "./stepHook.BHMagoq0.js";
     fetch(link.href, fetchOpts);
   }
 })();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguQ1dCNllpQ1QuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYm53ZlBFWHQuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
